9d001f4b96bd3eddec3ddc2554377da40c48ad5f
ltfschoen <ltfschoen@gmail.com>
2014-10-08 00:18:59 +1100

    Assigned to vars to avoid global variables. Added map bounds event listener. Refactored.

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -16,15 +16,25 @@
 
           console.log('Myhealth.directives - initialize method called');
 
+          var mapElement = $element[0];
+          // Third arg is noWrap boolean that forces Lat -90 to +90 and Lng -180 to +180
+          var latLng = new google.maps.LatLng(43.07493, -89.381388, true);
+          
           var mapOptions = {
-            center: new google.maps.LatLng(43.07493, -89.381388),
+            center: latLng,
             zoom: 16,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           };
 
-          var map = new google.maps.Map($element[0], mapOptions);
+          var map = new google.maps.Map(mapElement, mapOptions);
     
           $scope.onCreate({map: map});
+
+          // Check when map initialized only once and remove event listener after its run
+          var mapReady = google.maps.event.addListenerOnce(map, 'bounds_changed', function() {
+            var bounds = map.getBounds();
+            console.log('Myhealth.directives - map bounds initialized');
+          });
 
           // Create a marker and instantly add it to a map 
           var marker = new google.maps.Marker({
@@ -56,25 +66,21 @@
             fillOpacity: 0.3,
             map: map
           });
-
-          google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+          // Note: Event listeners assigned to a variable to prevent global variables
+          var placeMarker = google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
             console.log('Myhealth.directives - mousedown method called');
             // Stop the side bar from dragging when mousedown/tapdown on the map
             e.preventDefault();
-
             // Add marker to map
             marker.setMap(map);
-
             return false;
           });
 
           // Listen for marker click event
           var handleMarker = google.maps.event.addDomListener(marker, 'click', function (e) {
             console.log('Myhealth.directives - click marker method called');
-
             // Add InfoWindow to map
             infoWindow.open(map, marker);
-
             // Assign the returned MouseEvent object property of e
             var positionClicked = marker.getPosition();
             console.log('Marker PositionClicked is: ' + positionClicked);
@@ -85,16 +91,13 @@
           // Listen for polyline click event
           var handlePolyline = google.maps.event.addDomListener(polygon, 'click', function (e) {
             console.log('Myhealth.directives - click polygon method called');
-
             // Assign the returned MouseEvent object property of e
             var positionClicked = e.latLng;
             console.log('Polygon PositionClicked is: ' + positionClicked);
-
             return false;
           });
 
         }
-
         google.maps.event.addDomListener(window, 'load', initialize);
       }
     };
