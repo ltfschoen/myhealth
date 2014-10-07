e8ff1c381a55001575c50b9096ac4d03edb7b134
ltfschoen <ltfschoen@gmail.com>
2014-10-07 13:11:07 +1100

    Added InfoWindow on Marker click event

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,11 +9,20 @@
     $scope.googleMapsAPI = GoogleMapsAPI.query();
     console.log($scope.googleMapsAPI);
 
+    // Create a marker and instantly add it to a map 
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(43.07493, -89.381388),
+      icon: '../images/star_health_high.png',
+      shadow: '../images/star_health_high_shadow.png',
+      title: 'Star'
+    });
+
     $scope.mapCreated = function(map) {
       console.log('Myhealth.controllers - mapCreated method called');
       $scope.map = map;
     };
 
+    // Called from footer 
     $scope.centerOnMe = function () {
       console.log('Myhealth.controllers - centerOnMe method called');
       console.log('Centering');
