213562ecfe02744b8da7a63a8d47f0ea6d715df9
ltfschoen <ltfschoen@gmail.com>
2014-10-08 22:03:18 +1100

    Attempted to Toggle Panorama display
    Communication between Directive and Controller using $emit, $broadcast, and $on
    Problem is that $broadcast does not trigger Panorama with $emit. User must click the Marker twice.
    Problem is that if Panorama is hidden and the User clicks the Marker, the Panorama does not display.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,6 +9,34 @@
     $scope.googleMapsAPI = GoogleMapsAPI.query();
     console.log($scope.googleMapsAPI);
 
+    // Toggle the Panorama view visibility
+    $scope.visiblePano = false;
+    $scope.panoValue = 'Show';
+
+    $scope.togglePano = function(data) {
+      console.log('togglePano method called');
+      console.log('data is: ' + data);
+      $scope.visiblePano = !$scope.visiblePano;
+      // Enable display of Panorama if user clicks marker
+      if (data == true) { 
+        $scope.visiblePano = true;
+        // Broadcast to child directive that Panorama view has loaded 
+        console.log('Broadcasting to child directive that Panorama view has loaded');
+        $scope.$broadcast('loadedPano', true);
+        $scope.panoValue = 'Hide';
+      } else if ($scope.visiblePano == true) {
+        $scope.panoValue = 'Hide';
+      } else {
+        $scope.panoValue = 'Show';
+      }
+    }
+
+    // Register listener on parent controller
+    $scope.$on('togglePanoEvent', function(event, data) {
+      console.log('togglePanoEvent data sent to parent MapCtrl is: ' + data);
+      $scope.togglePano(data);
+    });
+
     $scope.mapCreated = function(map) {
       console.log('Myhealth.controllers - mapCreated method called');
       $scope.map = map;
